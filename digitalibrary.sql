0db6de52d93b561220c8edf3b946b1b17518cb8c
syafnaa <syafnamarwan@gmail.com>
2024-04-24 11:32:23 +0700

    DOKUMENTASI, DAN DATABASE

---
+++ digitalibrary.sql
@@ -0,0 +1,331 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 24, 2024 at 06:23 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `digitalibrary`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `buku`
+--
+
+CREATE TABLE `buku` (
+  `bukuID` int(11) NOT NULL,
+  `foto` varchar(255) DEFAULT NULL,
+  `judul` varchar(50) DEFAULT NULL,
+  `penulis` varchar(50) DEFAULT NULL,
+  `penerbit` varchar(50) DEFAULT NULL,
+  `tahunTerbit` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `buku`
+--
+
+INSERT INTO `buku` (`bukuID`, `foto`, `judul`, `penulis`, `penerbit`, `tahunTerbit`) VALUES
+(9, 'Gadis Kretek.jpg', 'Gadis Kretek', 'Ratih Kumala', 'Gramedia Pustaka Utama', '2019-07-07'),
+(10, 'Chasing Unicorn.jpg', 'Chasing Unicorns', 'Nicko Widjaja', 'Kepustakaan Populer Gramedia', '2023-08-01'),
+(11, 'Outliers.jpg', ' Outliers', ' Malcolm Gladwell', 'Sinar Star Book', '2008-12-01'),
+(12, 'Solo Leveling.jpg', 'Solo Leveling', 'DUBU/CHUGONG', 'm&c!', '2024-02-15'),
+(13, 'Ragna Crimson.jpeg', 'Ragna Crimson', 'Daiki Kobayashi', 'm&c!', '2024-03-23');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kategoribuku`
+--
+
+CREATE TABLE `kategoribuku` (
+  `kategoriID` int(11) NOT NULL,
+  `namaKategori` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `kategoribuku`
+--
+
+INSERT INTO `kategoribuku` (`kategoriID`, `namaKategori`) VALUES
+(4, 'Novels'),
+(5, 'Comedy'),
+(6, 'Psikologi'),
+(7, 'Manga'),
+(8, 'Aksi');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kategoribuku_relasi`
+--
+
+CREATE TABLE `kategoribuku_relasi` (
+  `kategoribukuID` int(11) NOT NULL,
+  `kategoriID` int(11) DEFAULT NULL,
+  `bukuID` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `kategoribuku_relasi`
+--
+
+INSERT INTO `kategoribuku_relasi` (`kategoribukuID`, `kategoriID`, `bukuID`) VALUES
+(19, 4, 9),
+(20, 5, 10),
+(12, 6, 11),
+(14, 7, 13),
+(21, 8, 12);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `koleksipribadi`
+--
+
+CREATE TABLE `koleksipribadi` (
+  `koleksiID` int(11) NOT NULL,
+  `userID` int(11) DEFAULT NULL,
+  `bukuID` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `koleksipribadi`
+--
+
+INSERT INTO `koleksipribadi` (`koleksiID`, `userID`, `bukuID`) VALUES
+(13, 1, 13),
+(10, 9, 11);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `peminjaman`
+--
+
+CREATE TABLE `peminjaman` (
+  `peminjamanID` int(11) NOT NULL,
+  `userID` int(11) DEFAULT NULL,
+  `bukuID` int(11) DEFAULT NULL,
+  `tanggalPeminjaman` date DEFAULT NULL,
+  `tanggalPengembalian` date DEFAULT NULL,
+  `jml_pinjam` int(5) DEFAULT NULL,
+  `statusPeminjaman` enum('dipinjam','dikembalikan') DEFAULT 'dipinjam'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `peminjaman`
+--
+
+INSERT INTO `peminjaman` (`peminjamanID`, `userID`, `bukuID`, `tanggalPeminjaman`, `tanggalPengembalian`, `jml_pinjam`, `statusPeminjaman`) VALUES
+(9, 1, 9, '2024-04-23', '2024-04-24', 1, 'dikembalikan'),
+(14, 9, 10, '2024-04-23', '2024-04-25', 1, 'dikembalikan'),
+(15, 1, 12, '2024-04-23', '2024-04-25', 1, 'dikembalikan'),
+(17, 1, 10, '2024-04-23', '2024-04-25', 2, 'dikembalikan'),
+(18, 1, 11, '2024-04-23', '2024-04-25', 1, 'dipinjam'),
+(19, 9, 13, '2024-04-23', '2024-04-25', 1, 'dipinjam');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ulasanbuku`
+--
+
+CREATE TABLE `ulasanbuku` (
+  `ulasanID` int(11) NOT NULL,
+  `userID` int(11) DEFAULT NULL,
+  `bukuID` int(11) DEFAULT NULL,
+  `ulasan` text DEFAULT NULL,
+  `rating` int(5) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `ulasanbuku`
+--
+
+INSERT INTO `ulasanbuku` (`ulasanID`, `userID`, `bukuID`, `ulasan`, `rating`) VALUES
+(4, 1, 9, 'Mantap', 8),
+(9, 1, 11, 'biasa aja', 6),
+(11, 9, 13, 'seru', 7),
+(14, 17, 11, 'ffgb', 4);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `userID` int(11) NOT NULL,
+  `username` varchar(50) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `namaLengkap` varchar(255) DEFAULT NULL,
+  `alamat` text DEFAULT NULL,
+  `level` enum('admin','petugas','peminjam') DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`userID`, `username`, `password`, `email`, `namaLengkap`, `alamat`, `level`) VALUES
+(1, 'syafna', '9cdfdc6fc77ab35d2072cc55e60ee629', 'syafna@gmail.com', 'syafna Marwa', 'Metro', 'peminjam'),
+(3, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'admin@gmail.com', 'admin', 'Balam', 'admin'),
+(4, 'petugas', 'afb91ef692fd08c445e8cb1bab2ccf9c', 'petugas@gmail.com', 'petugas', 'Pekalongan', 'petugas'),
+(9, 'jay', 'baba327d241746ee0829e7e88117d4d5', 'jay@gmail.com', 'jay', 'Jakarta', 'peminjam'),
+(16, 'fg', '633de4b0c14ca52ea2432a3c8a5c4c31', 'petugas@gmail.com', 'gfg', 'fg', 'peminjam'),
+(17, 'ardi', '0264391c340e4d3cbba430cee7836eaf', 'ardi@gmail.com', 'ardi', 'Metro', 'peminjam'),
+(18, 'dfd', 'eff7d5dba32b4da32d9a67a519434d3f', 'df@gmail.com', 'fg', 'Balam', 'petugas');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `buku`
+--
+ALTER TABLE `buku`
+  ADD PRIMARY KEY (`bukuID`);
+
+--
+-- Indexes for table `kategoribuku`
+--
+ALTER TABLE `kategoribuku`
+  ADD PRIMARY KEY (`kategoriID`);
+
+--
+-- Indexes for table `kategoribuku_relasi`
+--
+ALTER TABLE `kategoribuku_relasi`
+  ADD PRIMARY KEY (`kategoribukuID`),
+  ADD KEY `kategoriID` (`kategoriID`,`bukuID`),
+  ADD KEY `bukuID` (`bukuID`);
+
+--
+-- Indexes for table `koleksipribadi`
+--
+ALTER TABLE `koleksipribadi`
+  ADD PRIMARY KEY (`koleksiID`),
+  ADD KEY `userID` (`userID`,`bukuID`),
+  ADD KEY `bukuID` (`bukuID`);
+
+--
+-- Indexes for table `peminjaman`
+--
+ALTER TABLE `peminjaman`
+  ADD PRIMARY KEY (`peminjamanID`),
+  ADD KEY `userID` (`userID`,`bukuID`),
+  ADD KEY `bukuID` (`bukuID`);
+
+--
+-- Indexes for table `ulasanbuku`
+--
+ALTER TABLE `ulasanbuku`
+  ADD PRIMARY KEY (`ulasanID`),
+  ADD KEY `userID` (`userID`,`bukuID`),
+  ADD KEY `bukuID` (`bukuID`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`userID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `buku`
+--
+ALTER TABLE `buku`
+  MODIFY `bukuID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT for table `kategoribuku`
+--
+ALTER TABLE `kategoribuku`
+  MODIFY `kategoriID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `kategoribuku_relasi`
+--
+ALTER TABLE `kategoribuku_relasi`
+  MODIFY `kategoribukuID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+
+--
+-- AUTO_INCREMENT for table `koleksipribadi`
+--
+ALTER TABLE `koleksipribadi`
+  MODIFY `koleksiID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT for table `peminjaman`
+--
+ALTER TABLE `peminjaman`
+  MODIFY `peminjamanID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- AUTO_INCREMENT for table `ulasanbuku`
+--
+ALTER TABLE `ulasanbuku`
+  MODIFY `ulasanID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `kategoribuku_relasi`
+--
+ALTER TABLE `kategoribuku_relasi`
+  ADD CONSTRAINT `kategoribuku_relasi_ibfk_1` FOREIGN KEY (`kategoriID`) REFERENCES `kategoribuku` (`kategoriID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `kategoribuku_relasi_ibfk_2` FOREIGN KEY (`bukuID`) REFERENCES `buku` (`bukuID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `koleksipribadi`
+--
+ALTER TABLE `koleksipribadi`
+  ADD CONSTRAINT `koleksipribadi_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `user` (`userID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `koleksipribadi_ibfk_2` FOREIGN KEY (`bukuID`) REFERENCES `buku` (`bukuID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `peminjaman`
+--
+ALTER TABLE `peminjaman`
+  ADD CONSTRAINT `peminjaman_ibfk_1` FOREIGN KEY (`bukuID`) REFERENCES `buku` (`bukuID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `peminjaman_ibfk_2` FOREIGN KEY (`userID`) REFERENCES `user` (`userID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `ulasanbuku`
+--
+ALTER TABLE `ulasanbuku`
+  ADD CONSTRAINT `ulasanbuku_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `user` (`userID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `ulasanbuku_ibfk_2` FOREIGN KEY (`bukuID`) REFERENCES `buku` (`bukuID`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
